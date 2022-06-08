9d641c6278ca63ccd826b8b136a37ff94c4c67bc
sunshay <sunshayina@outlook.com>
2022-06-07 20:55:00 -0700

    argv

--- sys.py
+++ sys.py
@@ -1,10 +1,16 @@
 import sys
 #print(sys.argv[0])
 #print(len(sys.argv))
-def convert_distance(miles):
-    	return miles * 0.62137119  #conversion_factor = 0.6213...
-kilometers = convert_distance(int(sys.argv[1]))
+print(sys.argv) #print name of the file 
+# def convert_distance(miles):
+#     	return miles * 0.62137119  #conversion_factor = 0.6213...
+# kilometers = convert_distance(int(sys.argv[1]))
 
-#y = int(sys.argv[2])
- #miles = x + y
-print('the distance in miles is ', kilometers)
+# #y = int(sys.argv[2])
+#  #miles = x + y
+# print('the distance in miles is ', kilometers)
+print(help.__str__)
+# print(help.__dict__)
+print(help.__init__)
+# print(help.__class__)
+print(help.__eq__)
